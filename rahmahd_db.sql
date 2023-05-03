9b1f9d44cda90297b891a9a79d8d3276c6244d73
DarkMode49 <prmobin4sh@gmail.com>
2023-05-03 21:00:49 +0430

    Database designed Added: rahmahd_db.sql

---
+++ rahmahd_db.sql
@@ -0,0 +1,66 @@
+USE rahmahd_db;
+
+-- admin_users
+username        VARCHAR(16) UNIQUE
+password        VARCHAR(64)
+status          BOOLEAN DEFAULT 1
+
+-- manager_users
+id              INT(11) UNIQUE AUTO_INCREMENT
+username        VARCHAR(16) UNIQUE
+password        VARCHAR(64)
+kd_id           INT(11)
+full_name       VARCHAR(64)
+
+-- request_kd
+id              INT(11) UNIQUE AUTO_INCREMENT
+manager_user_id INT(11)
+kd_name         VARCHAR(64)
+address         TEXT
+status_code     TINYINT(3)
+
+-- kindergartens
+id              INT(11) UNIQUE AUTO_INCREMENT
+name            VARCHAR(64) UNIQUE
+location        POINT
+rate            FLOAT
+opening_date    DATE
+work_hour_start TIME
+work_hour_end   TIME
+address         TEXT UNIQUE
+telephone       VARCHAR(11)
+capacity        TINYINT(3) DEFAULT 0
+max_capacity    TINYINT(3)
+
+-- kids
+id              INT(11) UNIQUE AUTO_INCREMENT
+first_name      VARCHAR(32)
+last_name       VARCHAR(32)
+age             TINYINT(3)
+telephone       VARCHAR(11)
+gender          BOOLEAN
+national_id     VARCHAR(10)
+kd_id           INT(11)
+
+-- payment_factors
+id              INT(11) UNIQUE AUTO_INCREMENT
+kd_id           INT(11)
+title           VARCHAR(128)
+creation_date   DATE
+value_total     BIGINT(11)
+discount_total  BIGINT(11)
+final_value     BIGINT(11)
+kid_id          INT(11)
+status          TINYINT(3)
+
+-- kd_images
+kd_id           INT(11)
+first_name      VARCHAR(256) UNIQUE
+
+-- manager_user_login
+id              INT(11) UNIQUE
+token           VARCHAR(32) UNIQUE
+
+-- admin_user_login
+username        VARCHAR(16) UNIQUE
+token           VARCHAR(32) UNIQUE
